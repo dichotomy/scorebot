857275acd8314b556c00270bbe7172382edbb3c4
gi0cann <gionnec@gmail.com>
2014-01-12 20:11:10 -0500

    test

--- Scoreboard.py
+++ Scoreboard.py
@@ -394,7 +394,7 @@ class Scoreboard(threading.Thread):
                 this_round = round_score[0]
                 score = round_score[1]
                 scores += self.latest_scores[team] % (this_round, team, score)
-                self.rrd_update(team, score)
+                self.rrd_update(str(team), score)
                 self.rrd_graph(team)
                 team_imgfilename = "%s.png" % team
                 graphs += graph_line % team_imgfilename
