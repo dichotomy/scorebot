e7134a7e6344dda7f5150d4f4371a78dab25e712
Zach Hilbert <zachhilbert@gmail.com>
2017-02-08 22:31:37 -0500

    updating

--- Scorebot3/sbeapi/views/gameviews.py
+++ Scorebot3/sbeapi/views/gameviews.py
@@ -1,5 +1,3 @@
-import scorebot.utils.log as logger
-
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseBadRequest,\
         HttpResponseNotAllowed
