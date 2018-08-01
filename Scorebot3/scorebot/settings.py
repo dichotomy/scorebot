112885ea8b47dc55f402fdd1566827872eaa87f5
David Tomaschik <david@systemoverlord.com>
2018-08-01 14:14:03 -0700

    Add support for a local_settings settings module.

--- Scorebot3/scorebot/settings.py
+++ Scorebot3/scorebot/settings.py
@@ -82,3 +82,9 @@ AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',},
     {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',},
 ]
+
+# Allow local settings without modifying this file
+try:
+    from local_settings import *
+except ImportError:
+    pass
