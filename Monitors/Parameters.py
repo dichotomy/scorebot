834d06cedef7efba183f66d04215c984aa356a8f
Daniel Roberson <daniel@planethacker.net>
2018-11-06 10:43:23 -0800

    unused members, dict cleanup

--- Monitors/Parameters.py
+++ Monitors/Parameters.py
@@ -1,24 +1,21 @@
 #!/usr/bin/env python2.7
 
 class Parameters(object):
-    # TODO Document methods and attributes
     def __init__(self):
         self.debug = False
         self.timeout = 90
         # TODO read this from a file so its not exposed in repository
-        # IDEA - configuration file??
         self.sbe_auth = "124dd9b2-b89c-444b-9b15-30dc7bdd83bf"
         self.sb_ip = "10.150.100.81"
         self.sb_port = 80
         self.url = "/api/job"
-        self.reason = ""
-        self.headers = {}
-        self.headers["Connection"] = "keep-alive"
-        #self.headers["Host"] = self.sb_ip
-        self.headers["Accept-Encoding"] = "gzip, deflate"
-        self.headers["User-Agent"] = "Scorebot Monitor/3.0.0"
-        self.headers["SBE-AUTH"] = self.sbe_auth
-        self.headers["Accept"] = "*/*"
+        self.headers = {
+            "Connection" : "keep-alive",
+            "Accept-Encoding" : "gzip, deflate",
+            "User-Agent" : "Scorebot Monitor/3.0.0",
+            "SBE-AUTH" : self.sbe_auth,
+            "ACCEPT" : "*/*"
+        }
         self.scheme = "http"
 
     def get_headers(self):
