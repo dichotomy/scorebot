58cc49b33fa566c768cd47fbd01f43d295f14b7c
Daniel Roberson <daniel@planethacker.net>
2018-11-06 11:56:06 -0800

    add get_headers

--- Monitors/common.py
+++ Monitors/common.py
@@ -9,3 +9,8 @@ def errormsg(message):
 def no_unicode(text):
     return text.encode("utf-8") if isinstance(text, unicode) else text
 
+def get_headers(headers):
+    header_txt = ""
+    for header in headers:
+        header_txt += "%s: %s\r\n" % (header, headers[header])
+    return header_txt
