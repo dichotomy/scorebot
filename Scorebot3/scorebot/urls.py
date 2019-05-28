dd783fdb511228ddb5c945c94a9160f2250b1285
iDigitalFlame <idf@idigitalflame.com>
2019-05-28 13:01:06 -0400

    Update Scorebot3 folder with current version

--- Scorebot3/scorebot/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.conf.urls import url, include
-
-urlpatterns = [
-    url(r"^admin/", admin.site.urls),
-    url(r"^api/", include("scorebot_api.api", namespace="scorebot3_api")),
-    url(r"^", include("scorebot_api.urls", namespace="scorebot3")),
-]
--- Scorebot3/scorebot_api/__init__.py
+++
@@ -1,1 +0,0 @@
-default_app_config = 'scorebot.utils.apps.ScorebotAPI'
